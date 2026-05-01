9653df481027e1363752be62291295a52c6825cb
gr441_koaal <kulmanakovap@gmail.com>
2026-05-01 20:40:59 +0700

    add register

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,26 +1,26 @@
+
 describe('Модуль регистрации и добавления роли студента', () => {
-it('Регистрация нового пользователя и выбор роли "Студент"', () => {
-// Генерируем уникальные данные, чтобы не было конфликта
-const uniqueId = Date.now()
-const email = `qwe@gmail.com`
-const password = '123Ghu'
-const login = 'mira'
+    it('Регистрация нового пользователя и выбор роли "Студент"', () => {
+        const uniqueId = Date.now()
+        const login = `gghh`
+        const email = `${login}@testhh12.ru`
+        const password = 'hhHKL76'
 
-cy.visit('https://dev.profteam.su/registration') // предположим, что страница регистрации /register
+        cy.visit('https://dev.profteam.su/registration')
+        cy.get('input[autocomplete="username"]').type(login)
+        cy.get('input[autocomplete="email"]').type(email)
+        cy.get('input[autocomplete="new-password"]').first().type(password)
+        cy.get('input[autocomplete="new-password"]').last().type(password)
 
+        // Кнопка "Далее" (она видима)
+        cy.contains('button', 'Далее').click()
+        cy.get('input[autocomplete="family-name"]').should('be.visible').type('7')
+        cy.get('input[autocomplete="given-name"]').should('be.visible').type('г')
+        cy.get('input[autocomplete="additional-name"]').should('be.visible').type('7')
 
-cy.get('input[autocomplete="username"]').type(login)
-cy.get('input[autocomplete="email"]').type(email)
-cy.get('input[autocomplete="new-password"]').type(password)
-cy.get('input[autocomplete="new-password"]').type(password)
-cy.get('button[autocomplete="submit"]').click()
+        cy.contains('button', 'Создать аккаунт').click()
+        cy.url().should('include', '/account/main')
+        cy.contains('Личный кабинет', { timeout: 10000 }).should('be.visible')
 
-// После регистрации возможно нужно выбрать роль
-cy.contains('Студент').click() // или cy.get('[value="student"]').click()
-cy.get('button').contains('Сохранить').click()
-
-// Проверяем, что перебросило в личный кабинет
-cy.url().should('include', '/cabinet')
-cy.contains('Добро пожаловать').should('be.visible')
-})
+    })
 })
